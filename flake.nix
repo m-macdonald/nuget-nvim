e0178315fa1814b4855743c0bdf993391acc06b3
m-macdonald <37754975+m-macdonald@users.noreply.github.com>
2024-11-14 21:02:40 -0500

    Finally figured out how to change the binary name of the dev shell's nvim

--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
                 plugins.lazy = {
                     enable = true;
                     plugins = [
+                        pkgs.vimPlugins.plenary-nvim
                         {
                             name = "nuget-nvim";
                             dev = true;
@@ -39,7 +40,16 @@
 
         in {
             devShells.default = pkgs.mkShell {
-                packages = [ nvim pkgs.nuget ];
+                packages = [ 
+                    pkgs.nuget
+                    # Future me: DO NOT TOUCH THIS
+                    # It took ages to figure out how to allow two binaries with the same name to be on the path in the nix develop shell
+                    # This takes the nvim derivation that we build above and changes the binary name to 'devvim' so that my fully configured nvim isn't overridden by this derivation which is only used for testing.
+                    (pkgs.runCommand "devvim" { nativeBuildInputs = [ pkgs.makeWrapper ];} ''
+                        mkdir -p $out/bin
+                        makeWrapper ${nvim}/bin/nvim $out/bin/devvim --argv0 nvim
+                    '')
+                ];
             };
 
             format = pkgs.alejandra;
